bbc4d10818f60bea0b3b5c44058f73f2dd348f6b
Patrick Turley [pturley] <patrick.turley@gmail.com>
2012-03-26 13:04:53 +0200

    Added tastiness and presentation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ActiveRecord::Base
+
+  def valid?
+    make_tasty
+    super
+  end
+
+  private
+  
+  def make_tasty
+    self.description = self.description + " and it is tasty" if self.name.match(/sandwich/i)
+  end
 end
